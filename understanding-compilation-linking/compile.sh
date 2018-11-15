03c45329b509c48689cb27f17496670c3644774f
Bjorn Martens <bjorn@expeditious.nl>
2018-11-15 13:58:15 +0100

    Printed output clearer

--- understanding-compilation-linking/compile.sh
+++ understanding-compilation-linking/compile.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+red=`tput setaf 1`
+nocolor=`tput sgr0`
 gcc -O0 -std=c99 -g -c -o simple.o simple.c
+echo "${red}objdump -dr simple.o${nocolor}"
 objdump -dr simple.o
+echo "${red}nm simple.o${nocolor}"
+nm simple.o
 rm simple.o
+
