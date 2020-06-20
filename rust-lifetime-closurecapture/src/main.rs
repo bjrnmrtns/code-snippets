9b2abd9ee288620a129c9c0071d20f997a1acef2
Bjorn Martens <bjorn@expeditious.nl>
2020-06-20 10:38:16 +0200

    add closure vec and function for adding closures

--- rust-lifetime-closurecapture/src/main.rs
+++ rust-lifetime-closurecapture/src/main.rs
@@ -1,18 +1,23 @@
 struct ClosureHolder
 {
-    closure: Box<dyn FnMut()>,
+    closures: Vec<Box<dyn FnMut()>>,
 }
 
 impl ClosureHolder {
-    pub fn call(&mut self) {
-        self.closure.as_mut()()
+    pub fn call_all(&mut self) {
+        let x: () = self.closures.iter_mut().map(|c| { c.as_mut()(); }).collect();
+    }
+    pub fn add_closure(&mut self, closure: Box<FnMut()>) {
+        self.closures.push(closure);
     }
 }
 
 fn main() {
     let mut x = 3;
-    let mut holder = ClosureHolder { closure: Box::new(|| { x = x + 1; }) };
-    holder.call();
+    let mut holder = ClosureHolder { closures: Vec::new(), };
+    holder.add_closure(Box::new(|| { println!("hello"); }));
+    holder.add_closure(Box::new(|| { x = x + 1; }));
+    holder.call_all();
     println!("{}", x);
 }
 
