dd5a73961a7145ee3e32bd18c6db6dba5fbc0fda
Bjorn Martens <bjorn@expeditious.nl>
2020-06-20 10:42:20 +0200

    adde lifetime annotations

--- rust-lifetime-closurecapture/src/main.rs
+++ rust-lifetime-closurecapture/src/main.rs
@@ -1,13 +1,13 @@
-struct ClosureHolder
+struct ClosureHolder<'closure_lifetime>
 {
-    closures: Vec<Box<dyn FnMut()>>,
+    closures: Vec<Box<dyn FnMut() + 'closure_lifetime>>,
 }
 
-impl ClosureHolder {
+impl ClosureHolder<'_> {
     pub fn call_all(&mut self) {
         let x: () = self.closures.iter_mut().map(|c| { c.as_mut()(); }).collect();
     }
-    pub fn add_closure(&mut self, closure: Box<FnMut()>) {
+    pub fn add_closure<'closure_lifetime>(&mut self, closure: Box<FnMut() + 'closure_lifetime>) {
         self.closures.push(closure);
     }
 }
