ab5c59c8995025f0975666f3dd261f9b8a32dd27
Bjorn Martens <bjorn@expeditious.nl>
2020-06-20 12:01:34 +0200

    fixed up the example adding println will use borrowed value and not compile for good reason

--- rust-lifetime-closurecapture/src/main.rs
+++ rust-lifetime-closurecapture/src/main.rs
@@ -5,13 +5,10 @@ struct ClosureHolder<'closure_lifetime>
 
 impl<'closure_lifetime> ClosureHolder<'closure_lifetime> {
     pub fn call_all(&mut self) {
-        let x: () = self.closures.iter_mut().map(|c| { c.as_mut()(); }).collect();
-    }
-    pub fn add_closure(&mut self, closure: Box<FnMut() + 'closure_lifetime>) {
-        self.closures.push(closure);
+        let _: () = self.closures.iter_mut().map(|c| { c.as_mut()(); }).collect();
     }
 
-    pub fn add_closure2<CLOSURE: FnMut() + 'closure_lifetime>(&mut self, closure: CLOSURE) {
+    pub fn add_closure<CLOSURE: FnMut() + 'closure_lifetime>(&mut self, closure: CLOSURE) {
         self.closures.push(Box::new(closure));
     }
 }
@@ -19,8 +16,7 @@ impl<'closure_lifetime> ClosureHolder<'closure_lifetime> {
 fn main() {
     let mut x = 3;
     let mut holder = ClosureHolder { closures: Vec::new(), };
-    holder.add_closure(Box::new(|| { println!("hello"); }));
-    holder.add_closure2(|| { x = x + 1; });
+    holder.add_closure(|| { x = x + 1; });
     holder.call_all();
     //println!("{}", x);
 }
