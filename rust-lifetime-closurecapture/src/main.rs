4f5f187916078a7d617e86660723e964c79c58d5
Bjorn Martens <bjorn@expeditious.nl>
2020-06-20 10:56:34 +0200

    update lifetimes

--- rust-lifetime-closurecapture/src/main.rs
+++ rust-lifetime-closurecapture/src/main.rs
@@ -3,11 +3,11 @@ struct ClosureHolder<'closure_lifetime>
     closures: Vec<Box<dyn FnMut() + 'closure_lifetime>>,
 }
 
-impl ClosureHolder<'_> {
+impl<'closure_lifetime> ClosureHolder<'closure_lifetime> {
     pub fn call_all(&mut self) {
         let x: () = self.closures.iter_mut().map(|c| { c.as_mut()(); }).collect();
     }
-    pub fn add_closure<'closure_lifetime>(&mut self, closure: Box<FnMut() + 'closure_lifetime>) {
+    pub fn add_closure(&mut self, closure: Box<FnMut() + 'closure_lifetime>) {
         self.closures.push(closure);
     }
 }
