cf3e61ddc7039fa68bcfcb0dc7c6354e0b735745
Bjorn Martens <bjorn@expeditious.nl>
2020-06-22 20:28:33 +0200

    context example with closure

--- rust-lifetime-closurecapture/src/main.rs
+++ rust-lifetime-closurecapture/src/main.rs
@@ -1,22 +1,27 @@
-struct ClosureHolder<'closure_lifetime>
+struct ClosureHolder<'closure_lifetime, UIContext>
 {
-    closures: Vec<Box<dyn FnMut() + 'closure_lifetime>>,
+    closures: Vec<Box<dyn Fn(&mut UIContext) + 'closure_lifetime>>,
 }
 
-impl<'closure_lifetime> ClosureHolder<'closure_lifetime> {
-    pub fn call_all(&mut self) {
-        let _: () = self.closures.iter_mut().map(|c| { c(); }).collect();
+impl<'closure_lifetime, UIContext> ClosureHolder<'closure_lifetime, UIContext> {
+    pub fn call_all(&mut self, context: &mut UIContext) {
+        let _: () = self.closures.iter_mut().map(|c| { c(context); }).collect();
     }
 
-    pub fn add_closure<CLOSURE: FnMut() + 'closure_lifetime>(&mut self, closure: CLOSURE) {
+    pub fn add_closure<CLOSURE: Fn(&mut UIContext) + 'closure_lifetime>(&mut self, closure: CLOSURE) {
         self.closures.push(Box::new(closure));
     }
 }
 
+struct MyUIContext {
+    pub x: i32,
+}
+
 fn main() {
-    let mut x = 3;
-    let mut holder = ClosureHolder { closures: Vec::new(), };
-    holder.add_closure(|| {x = x + 1; });
-    holder.call_all();
+    let mut x = MyUIContext { x: 0 };
+    let mut holder: ClosureHolder<MyUIContext> = ClosureHolder { closures: Vec::new(), };
+    holder.add_closure(|x| { x.x = x.x + 1; });
+    holder.call_all(&mut x);
+    println!("{}", x.x);
 }
 
