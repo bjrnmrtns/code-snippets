ddfa80571e549e7c30a20b744772c1a4c373e2e7
Bjorn Martens <bjorn@expeditious.nl>
2021-05-27 17:23:03 +0200

    send more complex structs

--- rust-thread/src/main.rs
+++ rust-thread/src/main.rs
@@ -2,25 +2,49 @@ use std::sync::mpsc;
 use std::sync::mpsc::{Receiver, Sender};
 use std::thread;
 
+struct Vertex {
+    x: f32,
+    y: f32,
+    z: f32,
+}
+
+#[derive(Default)]
+struct Mesh {
+    pub v: Vec<Vertex>,
+    pub i: Vec<i32>,
+}
+
+struct Chunk {
+    pub id: (i32, i32, i32),
+}
+
+impl Default for Chunk {
+    fn default() -> Self {
+        Self {
+            id: (0, 0, 0)
+        }
+    }
+}
+
 fn main() {
-    let (txworker, rxmain): (Sender<i32>, Receiver<i32>) = mpsc::channel();
-    let (txmain, rxworker): (Sender<i32>, Receiver<i32>) = mpsc::channel();
+    let (txworker, rxmain): (Sender<Mesh>, Receiver<Mesh>) = mpsc::channel();
+    let (txmain, rxworker): (Sender<Chunk>, Receiver<Chunk>) = mpsc::channel();
 
     let child = thread::spawn(move || {
         loop {
-            let i = rxworker.recv().unwrap();
-            if i == 99 {
+            let chunk  = rxworker.recv().unwrap();
+            if chunk.id.0 == 99 {
                 break ();
             } else {
-                txworker.send(i).unwrap();
+                txworker.send(Mesh::default()).unwrap();
             }
         }
     });
     for i in 0..100 {
-        txmain.send(i).unwrap();
+        txmain.send(Chunk { id: (i, 0, 0) }).unwrap();
     }
     for i in 0..99 {
-        println!("{}", rxmain.recv().unwrap());
+        rxmain.recv().unwrap();
     }
     child.join().unwrap();
 }
