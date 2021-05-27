efb11b25012eddff43a2ab8eabb77f1db6bf304d
Bjorn Martens <bjorn@expeditious.nl>
2021-05-27 16:46:15 +0200

    implement simple channel with thread

--- rust-thread/src/main.rs
+++ rust-thread/src/main.rs
@@ -1,3 +1,26 @@
+use std::sync::mpsc;
+use std::sync::mpsc::{Receiver, Sender};
+use std::thread;
+
 fn main() {
-    println!("Hello, world!");
+    let (txworker, rxmain): (Sender<i32>, Receiver<i32>) = mpsc::channel();
+    let (txmain, rxworker): (Sender<i32>, Receiver<i32>) = mpsc::channel();
+
+    let child = thread::spawn(move || {
+        loop {
+            let i = rxworker.recv().unwrap();
+            if i == 99 {
+                break ();
+            } else {
+                txworker.send(i).unwrap();
+            }
+        }
+    });
+    for i in 0..100 {
+        txmain.send(i).unwrap();
+    }
+    for i in 0..99 {
+        println!("{}", rxmain.recv().unwrap());
+    }
+    child.join().unwrap();
 }
